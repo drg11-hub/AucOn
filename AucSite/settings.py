16dc59b4346e1e6ee7a69774539f080e7b238a79
DHARMIL GADA <60323052+drg11-hub@users.noreply.github.com>
2021-11-26 09:47:03 +0530

    Update settings.py

--- AucSite/settings.py
+++ AucSite/settings.py
@@ -141,8 +141,8 @@ MESSAGE_TAGS = {
     messages.ERROR: 'alert-danger',
  }
 
-EMAIL_USE_TLS=
-EMAIL_HOST = ''
-EMAIL_HOST_USER=''
-EMAIL_HOST_PASSWORD=''
-EMAIL_PORT=
+EMAIL_USE_TLS=True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER='itesttest4@gmail.com'
+EMAIL_HOST_PASSWORD='*temptest@4'
+EMAIL_PORT=587
